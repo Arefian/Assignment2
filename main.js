8887947f8f2166b97d1fbe446d2b84d1bfc80c93
ArefianDaniel <arefianscreed@gmail.com>
2020-03-31 10:26:33 -0400

    assignment2

--- main.js
+++ main.js
@@ -10,4 +10,20 @@ const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 thumbBar.appendChild(newImage);
 
+for (let x=1; x < 6; x++) {
+
+}
+
 /* Wiring up the Darken/Lighten button */
+
+function brightness() {
+    const dark = btn.getAttribute('class');
+    if (dark === 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent('lighten');
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    }
+}
+brightness();
+
+
